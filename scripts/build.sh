6f697d5edf4f35efa6f895859365735d1e9505cd
demo_knyang <demo.knyang@gmail.com>
2022-08-10 00:56:18 +0800

    patch statuscmd

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ROOT_PWD=$(dirname $(dirname "$0"))
+
+cd "$ROOT_PWD"
+
+if [ -d patches ] ; then
+	for patch in patches/* ; do
+		git apply "$patch" || exit 1
+	done
+fi
+
+cp -f cfg/blocks.h blocks.h 2>/dev/null
+
+sudo make clean install
+
+make clean
+rm blocks.h
